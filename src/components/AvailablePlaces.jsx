42876044ec1bdb3a7cd4ad75e46e3843f0b2d50e
rzkinr <rizkynurrokhim18@gmail.com>
2025-10-01 12:12:02 +0700

    transforming fetched data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
+import {sortPlacesByDistance} from '../loc.js';
 
 //const places = localStorage.getItem('places');
 
@@ -14,19 +15,26 @@ export default function AvailablePlaces({onSelectPlace}) {
       setIsFetching(true);
 
       try {
-        const response = await fetch('http://localhost:3000/placess');
+        const response = await fetch('http://localhost:3000/places');
         const resData = await response.json();
 
         if (!response.ok) {
           throw new Error('failed to fetch places');
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude,
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({message: error.message || 'Could not fetch places.'});
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
 
     fetchPlaces();
